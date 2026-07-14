ca83fba7f2d7091eaa466923eae24d9de2a49b32
ecotank <email.anda@domain.com>
2026-07-14 12:16:06 +0700

    penambahan aut selection dan ascreenshot artifacts

---
+++ tests/hello_aut.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+options = webdriver.FirefoxOptions()
+options.add_argument("--headless")
+
+
+driver = webdriver.Remote(
+    command_executor="http://localhost:4444/wd/hub",
+    options=options
+)
+
+
+try:
+    # membuka halaman AUT
+    driver.get("http://localhost:8080")
+
+    time.sleep(3)
+
+    # mengambil judul halaman
+    title = driver.title
+
+    print("Title halaman AUT:", title)
+
+    # validasi sederhana
+    assert title != ""
+
+    print("Testing AUT berhasil")
+
+
+finally:
+    driver.quit()
